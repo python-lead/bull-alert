c2b2c0478cb92894663fb8b30d161eca876076b7
Dawid Ryguła <dawid.rygula@.skygate.io>
2021-02-25 19:49:58 +0100

    [init] Create basic project structure

---
+++ backend/src/core/config.py
@@ -0,0 +1,12 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
---
+++ backend/src/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from src.core.api_routing import api_router
+from src.core.config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Root"}
