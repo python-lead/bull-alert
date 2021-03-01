03ce7dccc3683bde3e584496f8ed056609d2eb90
Dawid Ryguła <dawid.rygula@.skygate.io>
2021-03-01 17:38:42 +0100

    [BA2] Add testing script, update format.sh

--- backend/scripts/format.sh
+++ backend/scripts/format.sh
@@ -3,6 +3,6 @@ set -x
 
 isort --recursive  --force-single-line-imports --apply src
 autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place src --exclude=__init__.py
-black src
 isort --recursive --apply src
+black src
 flake8 src
