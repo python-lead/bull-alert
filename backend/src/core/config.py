568745c04e490015e47e61a33a0cccee7c4bc98c
Dawid Ryguła <rygula.dawid@gmail.com>
2021-11-02 15:55:45 +0100

    [BA4] Use lru_cache to get settings, add postgres env variables

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from pydantic import BaseSettings
 
 
@@ -5,10 +6,21 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str
 
+    # Etherscan
     ETHERSCAN_API_KEY: str
+
+    # Postgres
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
 
     class Config:
         case_sensitive = True
 
 
-settings = Settings()
+@lru_cache()
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
